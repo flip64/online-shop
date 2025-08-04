0b21f4a2d5d569732c98283d99b5acc36627e184
flip <jr64.naderloo@gmail.com>
2025-08-05 00:30:14 +0330

    edit header category

--- src/Components/Header/Des/HeaderCategoryMenu/HeaderCategoryMenu.js
+++ src/Components/Header/Des/HeaderCategoryMenu/HeaderCategoryMenu.js
@@ -5,96 +5,73 @@ import './HeaderCategoryMenu.css';
 
 const HeaderCategoryMenu = () => {
     const [categories, setCategories] = useState([]);
-    const [tmpcategories, settmpCategories] = useState([]);
-
-    const fetchData = async () => {
-        const response = await fetch("https://json.xstack.ir/api/v1/categories");
-        const data = await response.json();
-        console.log(data);
-        makeCategoryTree(data.data);
-        settmpCategories(data.data);
-    }
 
     useEffect(() => {
-        fetchData()
-    }, [])
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://127.0.0.1:8000/api/products/categories/");
+                const data = await response.json();
+                console.log(JSON.stringify(data, null, 2));
 
-    const makeCategoryTree = (categoryList) => {
-        console.log("makeCategoryTree");
-        console.log(categoryList);
-        let catList = [];
-        categoryList.length > 0 && (
-            categoryList.map((category) => {
-                if (category.parent_id === null) {
-
-                    category.children = [];
-                    category.hasSub = false;
-                    catList.push(category);
+                if (Array.isArray(data)) {
+                    setCategories(data);
+                } else if (data.data && Array.isArray(data.data)) {
+                    setCategories(data.data);
                 } else {
-                    let upd_obj = categoryList.findIndex((obj => obj.id == category.parent_id));
-                    catList[upd_obj].hasSub = true;
-                    catList[upd_obj].children.push(category);
+                    console.error("Unexpected API response structure:", data);
                 }
-            })
-        )
-        console.log(catList);
-        setCategories(catList);
-        
+            } catch (error) {
+                console.error("Error fetching categories:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const renderSubCategories = (subCategories) => {
+        return (
+            <ul>
+               
+                {subCategories.map((sub) => (
+                    <li key={sub.id}>
+                        <Link to={`/category/${sub.slug}`} className="page_14">
+                            {sub.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        );
+    };
+
+    if (categories.length === 0) {
+        return <div>در حال بارگذاری...</div>;
     }
 
-    const makeMenu = (catList) => {
-        console.log("makeMenu");
-        let res = catList.map(cat => {
-            <p>{cat.name}</p>
-        }); 
-        return res; 
-    }
-
-    if (categories.length == 0) {
-        return <div>Loading...</div>;
-    }
     return (
+        <ul className="menu-2 menu-custom menu-2 mega three-level" id="menu_header">
+            {categories.map((category) => (
+                <li key={category.id} className={category.children ? 'deep-3' : ''}>
+                    <Link title={category.slug} className="page_1 sf-with-ul" to={`/category/${category.slug}`}>
+                        {category.name}
+                        {category.children && category.children.length > 0 && <DownOutlined />}
+                    </Link>
 
-        <>
-            {categories.length > 0 && (
-                <ul className="menu-2 menu-custom menu-2 mega three-level" id="menu_header">
-                    {categories.map(category => (
-                        <li className={` ${category.hasSub ? 'deep-3' : ''}`} key={category.id}>
-                            <Link title={category.slug} className="page_1 sf-with-ul" to={`/category/${category.slug}`}>
-                                {category.name}
-                                <DownOutlined />
-                            </Link>
-                            {category.hasSub ?
-                                    category.children.length > 0 && (
-                                    <ul>
-                                        <div className="ul-title">
-                                            <span className="link-title"> {category.name}</span>
-                                            <Link to={`/category/${category.slug}`} className="more-items">
-                                                مشاهده همه محصولات {category.name}
-                                                <LeftOutlined />
-                                            </Link>
-                                        </div>
-                                        <li>
-                                            <a title="دسته بندی محصولات" className="page_11" href="#">دسته بندی محصولات</a>
-                                            <ul>
-                                                {category.children.map(subCategory => 
-                                                    <li key={subCategory.id}>
-                                                        <Link title={subCategory.slug} className="page_14" to={`/category/${subCategory.slug}`}>{subCategory.name}</Link>
-                                                    </li>
-                                                )}
-                                            </ul>
-                                        </li>
-                                        </ul>
-                                    )
-                                    :
-                                    ''
-                            }
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </>
-    )
-}
+                    {category.children && category.children.length > 0 && (
+                        <ul>
+                            <div className="ul-title">
+                                <span className="link-title">{category.name}</span>
+                                <Link to={`/category/${category.slug}`} className="more-items">
+                                    مشاهده همه محصولات {category.name}
+                                    <LeftOutlined />
+                                </Link>
+                            </div>
+                            {renderSubCategories(category.children)}
+                        </ul>
+                    )}
+                </li>
+            ))}
+        </ul>
+    );
+};
 
-export default HeaderCategoryMenu
+export default HeaderCategoryMenu;
