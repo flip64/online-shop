ed1b2d7178dbf399b6903bc22a4eda172c0dac8d
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-04 18:48:38 +0330

    Update HeaderCategoryMenu.js

--- src/Components/Header/Mob/HeaderCategoryMenu/HeaderCategoryMenu.js
+++ src/Components/Header/Mob/HeaderCategoryMenu/HeaderCategoryMenu.js
@@ -7,7 +7,7 @@ const HeaderCategoryMenu = ({ onClose }) => {
   const [openKeys, setOpenKeys] = useState([]);
 
   // آدرس API جنگو رو اینجا بذار
-  const apiUrl = "http://localhost:8000/api/categories/";
+  const apiUrl = "127.0.0.1:8000/api/products/categories";
 
   const fetchData = async () => {
     try {
