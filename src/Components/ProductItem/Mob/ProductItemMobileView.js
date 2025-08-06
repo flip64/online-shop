324470e7a0197ad9ab36ca24f74a5764e7b2a88f
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-06 15:05:11 +0330

    Update ProductItemMobileView.js

--- src/Components/ProductItem/Mob/ProductItemMobileView.js
+++ src/Components/ProductItem/Mob/ProductItemMobileView.js
@@ -2,53 +2,77 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { StarFilled } from '@ant-design/icons';
 import { seperatNumber, calcDiscount } from '../../../Utils/utilities';
-import './ProductItemMobileView.css'
+import './ProductItemMobileView.css';
 
-const ProductItemMobileView = ({product}) => {
+const ProductItemMobileView = ({ product }) => {
+    const price = product.price ?? 0;
+    const hasPrice = product.price !== undefined && product.price !== null;
+
     return (
-        <>
-            <div className="thumb">
-                <ul className="badges">
-                    <li className="badge-special"></li>
-                    <li className="badge-off"></li>
-                </ul>
+        <div className="thumb">
+            <ul className="badges">
+                <li className="badge-special"></li>
+                <li className="badge-off"></li>
+            </ul>
 
-                <div className="product-link">
-                    <Link to={`/product/${product.slug}`} title={product.name} className="image-product" >
-                        <img src={product.thumb} data-src={product.thumb} alt={product.slug} width="400" height="300" />
-                    </Link>
+            <div className="product-link">
+                <Link to={`/product/${product.slug}`} title={product.name} className="image-product">
+                    <img
+                        src={product.thumb}
+                        data-src={product.thumb}
+                        alt={product.slug || product.name}
+                        width="400"
+                        height="300"
+                    />
+                </Link>
 
-                    <Link className="h2" to={`/product/${product.slug}`} title={product.name}>
-                        {product.name}
-                    </Link>
+                <Link className="h2" to={`/product/${product.slug}`} title={product.name}>
+                    {product.name}
+                </Link>
+            </div>
+
+            <div className="details-price">
+                <div className="rate-section">
+                    <i className="shape-star">
+                        <StarFilled />
+                    </i>
+                    <span className="star-rate">3.34</span>
+                    <span className="reviewcount">(386)</span>
                 </div>
 
-                <div className="details-price">
-                    <div className="rate-section">
-                        <i className="shape-star">
-                            <StarFilled/>
-                        </i>
-                        <span className="star-rate"> 3.34</span>
-                        <span className="reviewcount">(386)</span>
-                    </div>
-
-                    <div className="price-area">
-                        <span className="old-price">
-                            <span className="old-price-inner">
-                                <span className="off-percent">{calcDiscount(product.price, product.price * 1.8, 1)}%</span>
-                                <span className="amount-old-price">{seperatNumber(product.price * 1.8)}</span>
-                            </span>
+                <div className="price-area">
+                    <span className="old-price">
+                        <span className="old-price-inner">
+                            {hasPrice ? (
+                                <>
+                                    <span className="off-percent">
+                                        {calcDiscount(price, price * 1.8, 1)}%
+                                    </span>
+                                    <span className="amount-old-price">
+                                        {seperatNumber(price * 1.8)}
+                                    </span>
+                                </>
+                            ) : (
+                                <span className="amount-old-price">نامشخص</span>
+                            )}
                         </span>
-                        <span className="price">
-                            <span className="price-amount">{seperatNumber(product.price)}</span>
-                            <span className="currency">تومان</span>
-                        </span>
-                        
-                    </div>
+                    </span>
+                    <span className="price">
+                        {hasPrice ? (
+                            <>
+                                <span className="price-amount">
+                                    {seperatNumber(price)}
+                                </span>
+                                <span className="currency">تومان</span>
+                            </>
+                        ) : (
+                            <span className="price-amount">قیمت ندارد</span>
+                        )}
+                    </span>
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
-export default ProductItemMobileView
+export default ProductItemMobileView;
