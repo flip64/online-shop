c79e75683d5f8aa92aa6b8ae11efcda595c14b81
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-07 10:11:27 +0330

    Update HomePage.js

--- src/Pages/Client/HomePage.js
+++ src/Pages/Client/HomePage.js
@@ -7,25 +7,23 @@ const HomePage = () => {
 
     const [products, setProducts] = useState([]);
     const [specialProducts, setSpecialProducts] = useState([]);
-  
+
     const fetchSpecialProducts = async () => {
-    try {
-        const response = await fetch("http://127.0.0.1:8000/api/products/specials");
-        const data = await response.json();
-        setSpecialProducts(data.results || []);  // طبق ساختار پاسخ API
-    } catch (error) {
-        console.error("خطا در دریافت محصولات ویژه:", error);
-    }
-};
-    
-    
-    
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/products/specials");
+            const data = await response.json();
+            setSpecialProducts(data.results || []);
+        } catch (error) {
+            console.error("خطا در دریافت محصولات ویژه:", error);
+        }
+    };
+
     const fetchData = async () => {
         try {
             const response = await fetch("http://127.0.0.1:8000/api/products/products?limit=15");
             const data = await response.json();
             console.log("Fetched data:", data);
-            setProducts(data); // اطمینان داری که data آرایه است؟
+            setProducts(data.results || []);
         } catch (error) {
             console.error("خطا در دریافت محصولات:", error);
         }
@@ -36,25 +34,20 @@ const HomePage = () => {
         fetchSpecialProducts();
     }, []);
 
-
-
-
-
-    
     return (
-    <>
-        <p>تعداد محصولات: {products.length}</p>
-        <div className='page_home'>
-            <div className='main-content'>
-                <Space direction="vertical" size="large" style={{ display: 'flex' }}>
-                    <SlideShow />
-                    <ProductsSwiperList title="محصولات ویژه" products={specialProducts} />
-                    <ProductsSwiperList title="جدیدترین محصولات" products={products} />
-                </Space>
+        <>
+            <p>تعداد محصولات: {products.length}</p>
+            <div className='page_home'>
+                <div className='main-content'>
+                    <Space direction="vertical" size="large" style={{ display: 'flex' }}>
+                        <SlideShow />
+                        <ProductsSwiperList title="محصولات ویژه" products={specialProducts} />
+                        <ProductsSwiperList title="جدیدترین محصولات" products={products} />
+                    </Space>
+                </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
+};
 
-    }
 export default HomePage;
