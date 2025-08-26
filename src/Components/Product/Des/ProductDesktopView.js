5bb8bc729ff61ca0b56adc03f7e321d05b0c40e7
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-26 19:59:48 +0330

    Update ProductDesktopView.js

--- src/Components/Product/Des/ProductDesktopView.js
+++ src/Components/Product/Des/ProductDesktopView.js
@@ -1,221 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Row, Image, Form, Select, InputNumber, Button, Tabs, Input, Rate } from 'antd';
-import { CopyOutlined, HeartOutlined, LeftOutlined, ShareAltOutlined, ShoppingOutlined, StarFilled } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
-import { seperatNumber, calcDiscount } from '../../../Utils/utilities';
-import ProductsSwiperList from '../../ProductsSwiperList/ProductsSwiperList';
-import FormItem from 'antd/es/form/FormItem';
-
-import './ProductDesktopView.css';
-import guarantyImg from '../../../Assets/img/guaranty.svg';
-import cartReturnImg from '../../../Assets/img/cart-return.svg';
-import deliveryFastImg from '../../../Assets/img/delivery-fast.svg';
-import paymentImg from '../../../Assets/img/payment.svg';
-import brand from '../../../Assets/img/samsung.png';
-import ProductCommentItems from '../../ProductCommentItems/ProductCommentItems';
-
-const { Option } = Select;
-const layout = {
-    labelCol: {
-        span: 8,
-    },
-    wrapperCol: {
-        span: 16,
-    },
-};
-const { TabPane } = Tabs;
-const ProductDesktopView = ({product , relatedProducts}) => {
-
-    const [visible, setVisible] = useState(false);
-    const [newPrice, setNewPrice] = useState(product.price);
-
-
-    useEffect(() => {
-        console.log('product');
-        console.log(product);
-        console.log('relatedProducts');
-        console.log(relatedProducts);
-        setNewPrice(product.price);
-    }, []); 
-
-    const items = [
-        {
-            key: '0',
-            label: 'توضیحات',
-            children: (
-                <Row>
-                    <Col
-                        xs={2}
-                        sm={4}
-                    ></Col>
-                    <Col xs={20} sm={16}>
-                        <div className="text-area">
-                            <h2>هندزفری AKG سامسونگ&nbsp;S10 - S9 - S8 -Note 8 - Note 9</h2>
-                            <p></p>
-                            <p></p>
-                            <p>در جعبه خرید گوشی های سامسونگ S8 وS8 Plus ، هندزفری مدل EO-IG955 وجود دارد. این هندزفری دارای طراحی زیبا و ارگونومیک حرفه ای است که با داشتن گوشی هایی به شکل کج به خوبی درون گوش قرار می گیرند.</p>
-                            <p>با توجه به نوع طراحی هندزفری سامسونگ s8، بدون ایجاد مزاحمت می توانید ساعت های طولانی از آن برای شنیدن موسیقی و مکالمه استفاده نمایید.</p>
-                            <p><span style={{ color: '#ff0000' }}><em>توجه: هندزفری </em><em>&nbsp;akg&nbsp;</em><em>سامسونگ برای تمام گوشی و تبلتهای دارای پورت 3.5</em><em>mm </em><em>&nbsp;مناسب بوده و کاملا سازگار است</em><em>.</em></span></p>
-                        </div>
-                    </Col>
-                    <Col
-                        xs={2}
-                        sm={4}
-                    ></Col>
-                </Row>
-            )
-        },
-        {
-            key: '1',
-            label: 'مشخصات محصول',
-            children: (
-                <Row>
-                    <Col
-                        xs={2}
-                        sm={4}
-                    ></Col>
-                    <Col xs={20} sm={16}>
-                        <div className="table-wrapper">
-                            <table className="table table-hover table-attributes">
-                                <tbody>
-                                    <tr>
-                                        <td className="first-td">
-                                            <span>مدل</span>
-                                        </td>
-                                        <td className="second-td"> EO-IG955 Tuned by AKG</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="first-td">
-                                            <span>نوع اتصال</span>
-                                        </td>
-                                        <td className="second-td"> با سیم جک 3.5 mm</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="first-td">
-                                            <span>وزن</span>
-                                        </td>
-                                        <td className="second-td"> 14.74 گرم</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="first-td">
-                                            <span>کیفیت خروجی صدا</span>
-                                        </td>
-                                        <td className="second-td"> فلت و شفاف</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="first-td">
-                                            <span>انواع دکمه های کنترلی</span>
-                                        </td>
-                                        <td className="second-td"> play/pause, volume</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </Col>
-                    <Col
-                        xs={2}
-                        sm={4}
-                    ></Col>
-                </Row>
-            )
-        },
-        {
-            key: '2',
-            label: 'نظرات',
-            children: (
-                <Row>
-                    <Col
-                        xs={2}
-                        sm={4}
-                    ></Col>
-                    <Col xs={20} sm={16}>
-                        <div className="rate" title="امتیاز 3.41 از 5 توسط 8981 کاربر">
-                            <div className="rating-container rating-md rating-animate">
-                                <div className="rating-stars" title="Three Stars">
-                                    <Rate allowHalf defaultValue={3.41} />
-                                </div>
-                            </div>
-                            <span>امتیاز 3.41 از 5 توسط 8981 کاربر</span>
-                        </div>
-
-                        <div id="comment_title">
-                            دیدگاه ها
-                        </div>
-
-                        <div className="clearfix" id="comment_area">
-                            <Form action="#" method="post" id="comment_form" className="form-horizontal  form-maker" noValidate>
-                                <Row>
-                                    <Col lg={10} md={10} sm={10} xs={24}>
-                                        <Form.Item label="نام و نام خانوادگی">
-                                            <Input placeholder="نام و نام خانوادگی خود را وارد کنید" size="large" />
-                                        </Form.Item>
-                                        <Form.Item label="ایمیل">
-                                            <Input placeholder="ایمیل خود را وارد کنید" type='email' size="large" />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col lg={14} md={14} sm={14} xs={24}>
-                                        <Form.Item label="دیدگاه خود را با ما در میان بگذارید">
-                                            <Input.TextArea placeholder="متن دیدگاه خود را بنویسید" />
-                                        </Form.Item>
-                                    </Col>
-
-                                </Row>
-                                <Form.Item dir={'ltr'}>
-                                    <Button name="save_comment" id="save_comment" label="" type="primary" htmlType="submit" className="btn" size="large">ارسال دیدگاه</Button>
-                                </Form.Item>
-
-                            </Form>
-                        </div>
-                        <ul id="comments">
-                            <ProductCommentItems comments={product.comments} />
-                        </ul>
-                    </Col>
-                    <Col
-                        xs={2}
-                        sm={4}
-                    ></Col>
-                </Row>
-            )
-        }
-
-    ]
-
-    if (product === undefined && relatedProducts === undefined) {
-        return <div>Loading ...</div>
-    }
-    return (
-        <>
-            <div className='sides'>
-                <div id="side_top">
-                    <div className="breadcrumbs clearfix">
-                        <LeftOutlined />
-
-                        <Link to="/">جانبی</Link>
-                        <LeftOutlined />
-
-                        <Link to="#">لوازم جانبی موبایل</Link>
-                        <LeftOutlined />
-
-                        <Link to="#">خرید لوازم جانبی سامسونگ Samsung</Link>
-                        <LeftOutlined />
-
-                        <Link to="#">خرید و لیست قیمت هندزفری بی سیم سامسونگ</Link>
-                        <LeftOutlined />
-
-                        هندزفری جک 3.5 میلیمتری akg سامسونگ Samsung EO-IG955 AKG Earphone
-                    </div>
-                </div>
-                <Row>
-                    <Col id="side_center" lg={24} xs={24}>
-                        <div id="box_product_details" className='box'>
-                            <div className='product mode-1'>
-                                <div className='body'>
-                                    <div className='body-top clearfix'>
-                                        <div className='section-right'>
-                                            <a className='image' >
-                                                <Image
                                                     preview={{
-                                                        visible: false,
+          import React, { useState, useEffect } from 'react';
+import { Image, InputNumber, Button, Rate } from 'antd';
+import { HeartOutlined, ShareAltOutlined, ShoppingOutlined } from '@ant-design/icons';
+import ProductsSwiperList from '../../ProductsSwiperList/ProductsSwiperList';
+import { seperatNumber, calcDiscount } from '../../../Utils/utilities';
+
+const ProductDesktopView = ({ product, relatedProducts }) => {
+  const price = parseFloat(product.base_price);
+  const [quantity, setQuantity] = useState(1);
+  const [newPrice, setNewPrice] = useState(price);
+
+  useEffect(() => {
+    setNewPrice(price * quantity);
+  }, [price, quantity]);
+
+  if (!product) return <div>Loading...</div>;
+
+  return (
+    <div className="product-desktop">
+      <h1>{product.name}</h1>
+      <p>{product.slug}</p>
+
+      <Image.PreviewGroup>
+        {product.images.map((img, index) => (
+          <Image key={index} src={img.image} alt={product.name} />
+        ))}
+      </Image.PreviewGroup>
+
+      <div className="price-section">
+        <InputNumber
+          min={1}
+          max={100}
+          value={quantity}
+          onChange={val => setQuantity(val)}
+        />
+        <span>{seperatNumber(newPrice)} تومان</span>
+        <span className="old-price">{seperatNumber(price * 1.8)}</span>
+        <span className="discount">{calcDiscount(price, price * 1.8, 1)}%</span>
+      </div>
+
+      <div className="actions">
+        <Button icon={<ShoppingOutlined />}>افزودن به سبد</Button>
+        <HeartOutlined />
+        <ShareAltOutlined />
+      </div>
+
+      <div className="related-products">
+        {relatedProducts.length > 0 && <ProductsSwiperList products={relatedProducts} />}
+      </div>
+
+      <div className="rating">
+        <Rate allowHalf defaultValue={product.rating || 0} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductDesktopView;                                              visible: false,
                                                     }}
                                                     src={product.image}
                                                     onClick={() => setVisible(true)}
