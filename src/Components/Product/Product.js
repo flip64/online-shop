f21e372a160c3edbf6fbce0b8ec3121f87c625b3
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-27 15:49:26 +0330

    Update Product.js

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -40,6 +40,8 @@ const Product = () => {
 
   const fetchRelatedProducts = async (categBASBASE_URLE_URLorySlug00) => {
     try {
+      url=`${BASE_URL}products/categories/${slug}&limit=15`
+      console.log(url)
       const response = await fetch(`${BASE_URL}products/categories/${slug}&limit=15`);
       if (!response.ok) throw new Error(`Failed to fetch related products: ${response.status}`);
       const data = await response.json();
